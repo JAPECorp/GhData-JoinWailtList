b664445b56f1f547d9448dac6cf57b9ba3a33870
Eugene <debraheug@gmail.com>
2022-06-14 17:18:05 +0000

    footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Index from '../components/landing-page/landing-page'
+import Footer from '../components/utilities/footer/footer'
 import Nav from '../components/utilities/nav/nav'
 
 
@@ -8,8 +9,9 @@ import Nav from '../components/utilities/nav/nav'
 export default function Home() {
   return (
     <>
-    <Nav/>
-    <Index/>
+      <Nav />
+      <Index />
+      <Footer />
     </>
   )
 }
