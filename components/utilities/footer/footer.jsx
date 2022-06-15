8a5755afd457ef608bb3f777dd3a09fbdf6be0fe
Eugene <debraheug@gmail.com>
2022-06-15 08:35:44 +0000

    footer

--- components/utilities/footer/footer.jsx
+++ components/utilities/footer/footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Image from "next/image";
 
 export default function Footer(props) {
     return (
-        <div>footer</div>
+        <footer className="p-4 bg-ghdata-section-gradient  md:flex md:items-center md:justify-between md:p-6">
+            <h1>LOGO</h1>
+            <span>GHData &copy; {new Date().getFullYear()}</span>
+        </footer>
     )
 }
